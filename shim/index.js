481c58460fc18a6ccef675a18ce5de1b0a5211d8
Sebastian Döll <sebastian@katallaxie.dev>
2025-01-08 12:46:10 +0100

    fix: shim loading binary

--- shim/index.js
+++ shim/index.js
@@ -1,21 +1,26 @@
 function chooseBinary() {
   if (platform === 'linux' && arch === 'x64') {
-      return `main-amd64-linux`
+    return `main-amd64-linux`
   }
 
   if (platform === 'linux' && arch === 'arm64') {
     return `main-arm64-linux`
-}
+  }
 
   if (platform === 'darwin' && arch === 'arm64') {
-      return `main-arm64-darwin`
+    return `main-arm64-darwin`
   }
 
   if (platform === 'darwin' && arch === 'x64') {
-      return `main-amd64-darwin`
+    return `main-amd64-darwin`
   }
 }
 
+const os = require('os');
+const childProcess = require('child_process');
+const resolve = require('path').resolve
+const platform = os.platform()
+const arch = os.arch()
 const binary = chooseBinary()
-const mainScript = `${__dirname}/dist/${binary}`
+const mainScript = resolve(`${__dirname}/../dist/${binary}`)
 const spawnSyncReturns = childProcess.spawnSync(mainScript, { stdio: 'inherit' })
